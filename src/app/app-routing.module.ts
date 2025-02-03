b2d2c80966bad1d15930a803bae3e461e041cbdf
JumelWilliam <williamjumel1998@gmail.com>
2025-02-03 12:33:40 +0100

    Ajout du header, footer et modifications graphiques

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Page d'accueil
+  { path: 'about', component: AboutComponent }, // Page À propos
+  { path: 'product', component: ProductComponent }, // Page Détail Produit
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
